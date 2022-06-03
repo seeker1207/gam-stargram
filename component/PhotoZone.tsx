6b689b69d2c51ff790cf5c75f5965ee533986d76
seeker1207 <jm3420@naver.com>
2022-06-03 11:39:33 +0900

    이미지 placeholder 적용

--- component/PhotoZone.tsx
+++ component/PhotoZone.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Grid, Image, SemanticWIDTHSNUMBER } from 'semantic-ui-react';
+import { Grid, Header, Image, Placeholder, SemanticWIDTHSNUMBER } from 'semantic-ui-react';
 import { v4 as uuid } from 'uuid';
+import styled from 'styled-components';
 
 interface Props {
   col: SemanticWIDTHSNUMBER;
@@ -8,13 +9,27 @@ interface Props {
   imgList: Array<String>;
 }
 
+const PlaceholderWrapper = styled(Placeholder)`
+  .image {
+    margin-top: 75%;
+    height: 0 !important;
+  }
+`;
+
 function PhotoZone({ col, imgList, row } : Props) {
   const getGridColumns = (rowImgList) => rowImgList
-    .map((img : string) => <Grid.Column key={uuid()}> <Image src={img} /> </Grid.Column>);
+    .map((img : string) => (
+      <Grid.Column key={uuid()}>
+        <PlaceholderWrapper><Placeholder.Image /></PlaceholderWrapper>
+        {/* <Image href="google.com" src={img} /> */}
+        <Header size="medium">이미지 제목</Header>
+      </Grid.Column>
+    ));
   const calculatedRow = row ?? (imgList.length / col) + 1;
 
   return (
     <Grid celled>
+
       {
         Array(calculatedRow).fill(null).map((_, idx) => (
           <Grid.Row key={uuid()} columns={col}>
@@ -27,6 +42,7 @@ function PhotoZone({ col, imgList, row } : Props) {
           </Grid.Row>
         ))
       }
+
     </Grid>
   );
 }
